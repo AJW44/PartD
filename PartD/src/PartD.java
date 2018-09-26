fb4a98338006363fc0a29d8fd2a6e666fe50339c
AJW44 <bookkeepingajw@gmail.com>
2018-09-26 16:13:59 +1200

    Fourth line of the assignment stating favourite day of the week

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -7,6 +7,7 @@ public class PartD {
 		System.out.println("Hi, my name is Amanda Wright\n");
 		System.out.println("My favourite colour is White\n");
 		System.out.println("My favourite mode of transporation is Car\n");
+		System.out.println("The best day of the week is Friday\n");
 	}
 
 }
