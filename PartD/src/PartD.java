e8743a3521f5087bb33ce3647c92efb649a40484
AJW44 <bookkeepingajw@gmail.com>
2018-09-26 16:11:23 +1200

    Third line of transport stating favourite mode of transport

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,7 +5,8 @@ public class PartD {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi, my name is Amanda Wright\n");
-		System.out.println("My favourite colour is White");
+		System.out.println("My favourite colour is White\n");
+		System.out.println("My favourite mode of transporation is Car\n");
 	}
 
 }
