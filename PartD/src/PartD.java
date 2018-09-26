9bda1c67c5f4a0811f19352cb13548213677243a
AJW44 <bookkeepingajw@gmail.com>
2018-09-26 16:07:29 +1200

    Second line of assignment stating favourite colour

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi, my name is Amanda Wright\n");
+		System.out.println("My favourite colour is White");
 	}
 
 }
